dd6fe8b32e2794b6df42d1f2f13998b4dcd81652
Narcis Brindusescu <Narcis75@gmail.com>
2023-01-16 23:06:26 +0200

    hai mai cu spor

--- backend/app/Controllers/Http/ClientsController.ts
+++ backend/app/Controllers/Http/ClientsController.ts
@@ -1,9 +1,24 @@
- //import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Validareemail from 'App/Models/Validareemail'
+import Mail from '@ioc:Adonis/Addons/Mail'
 
 export default class ClientsController {
 
-public async validare_email(){
-    console.log('Validare email!')
+public async validare_email({request}: HttpContextContract){
+    let {email} =request.body();
+    const random = Math.floor(Math.random() * (999999 - 100000 + 1)) + 100000;
+    let d = {cod:random.toString(),email}
+    await Validareemail.create(d);
+    //console.log('Validare email!',validare)
+
+    await Mail.send((message) => {
+        message
+          .from('programari@eleventen.ro')
+          .to(email)
+          .subject('Cod verificare email')
+          .htmlView('emails/validareemail', { cod:random.toString() })
+      })
+
     return 'Validare email!'
 }
 }
